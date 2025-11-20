f15bda9abd1b12226a7ec2ae6fcfe5ab7cadd5e5
gildevson@gmail.com <gilsonfonseca92@gmail.com>
2025-11-19 23:30:56 -0300

    Adicionando mais dados

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -10,17 +10,24 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
-menuItems = [
-  { nome: 'Usuários', rota: '/usuarios', icone: 'person' },
-  { nome: 'Produtos', rota: '/produtos', icone: 'inventory_2' },
-  { nome: 'Estoque', rota: '/estoque', icone: 'store' }, // ✅ novo item
-  { nome: 'Conta Contábil', rota: '/contas-contabeis', icone: 'account_balance' },
-  { nome: 'Relatórios', rota: '/relatorios', icone: 'bar_chart' },
-  { nome: 'Operações', rota: '/operacoes', icone: 'receipt_long' }
-];
+
+  menuItems = [
+    { nome: 'Usuários', rota: '/usuarios', icone: 'person' },
+    { nome: 'Produtos', rota: '/produtos', icone: 'inventory_2' },
+    { nome: 'Estoque', rota: '/estoque', icone: 'store' },
+    { nome: 'Conta Contábil', rota: '/contas-contabeis', icone: 'account_balance' },
+    { nome: 'Operações', rota: '/operacoes', icone: 'receipt_long' },
+    { nome: 'Relatórios', rota: '/relatorios', icone: 'bar_chart' }
+  ];
+
   constructor(private router: Router) {}
 
   navegar(rota: string) {
     this.router.navigate([rota]);
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/']);
   }
 }
